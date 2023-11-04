fc8773d47d3bba25b966cb048774f287f16d440d
technoryta <technoryta@gmail.com>
2023-11-04 16:11:41 -0700

    update code

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 
 def element_at(my_list, idx):
     """ Retrieves an element from a list"""
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx > (len(my_list) - 1):
         return None
     else:
         return my_list[idx]
