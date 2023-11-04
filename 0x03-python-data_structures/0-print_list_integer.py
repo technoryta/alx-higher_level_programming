b00ef7f526e9df78b65f4213d538d6d8579f1cb0
technoryta <technoryta@gmail.com>
2023-11-04 15:51:24 -0700

    update code

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """
+    Function that prints all integers in a list
+    """
+    for integ in my_list:
+        print("{:.0f}".format(integ))
