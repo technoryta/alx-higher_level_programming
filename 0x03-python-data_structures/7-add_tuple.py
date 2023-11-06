8b238a621a56a7e0a1614053c6e779a11f1b0053
technoryta <technoryta@gmail.com>
2023-11-06 15:35:18 -0800

    update code

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,6 +2,6 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     """Returns a tuple of 2 integers"""
-    tuple_a = tuple_a + (0,)
-    tuple_b = tuple_b + (0,)
+    tuple_a = tuple_a + (0,0)
+    tuple_b = tuple_b + (0,0)
     return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
