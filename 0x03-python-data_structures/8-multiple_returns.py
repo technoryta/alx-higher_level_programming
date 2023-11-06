e927d3929fa8e2787cc23b84aa09d7e5d3219f5b
technoryta <technoryta@gmail.com>
2023-11-06 15:45:53 -0800

    update code

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
     """Returns string length and first character"""
     if len(sentence) == 0:
-        return None
+        return (0, None)
     else:
         return ((len(sentence), sentence[0]))
