cd8d31d4d8e8dc070a03a236576be7bed6c4ebd3
technoryta <technoryta@gmail.com>
2023-11-08 18:38:32 -0800

    update code

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,4 +2,4 @@
 
 def search_replace(my_list, search, replace):
     """replaces all occurrences of an element by another in a new list"""
-    [replace if x == search else x for x in my_list]
+    return ([replace if x == search else x for x in my_list])
