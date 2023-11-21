04d5a7bd27b0761b265f030730353ab82865745e
technoryta <technoryta@gmail.com>
2023-11-20 19:55:50 -0800

    update code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,6 +5,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         while num is not x:
             print(my_list[num], end='')
+            num += 1
     except IndexError:
         None
     print()
