64fcfcf6521fc419f076c457891355767794c22b
technoryta <technoryta@gmail.com>
2023-11-20 20:43:48 -0800

    update code

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,9 +4,10 @@ def safe_print_list_list_integers(my_list=[], x=0):
     num = 0
     try:
         while num is not x:
-            print(my_list[num], end='')
+            print("{:d}".format(my_list[num]), end='')
             num += 1
     except (IndexError, ValueError, TypeError):
         pass
+
     print()
     return num
