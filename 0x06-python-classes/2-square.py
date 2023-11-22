634594ea1b6b4b071ebdd71fd10f349fbed5321f
technoryta <technoryta@gmail.com>
2023-11-22 15:56:21 -0800

    updated script

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Define a square"""
+
+
+class Square:
+    """Instantiate with option"""
+    def __init__(self, size=0):
+        """Initialise the square class
+
+            Args:
+                size(int): size of square
+
+            Raises:
+                TypeError: if size is not integer
+                ValueError: if size is negative
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be integer')
+        if size < 0:
+            raise ValueError('size must be >=0')
+        self.__size = size
