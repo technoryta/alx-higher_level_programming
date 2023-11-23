421d8d716e4b168e4344a205c02c952cb3a6e50f
technoryta <technoryta@gmail.com>
2023-11-22 17:49:27 -0800

    updated script

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -15,11 +15,6 @@ class Square:
                 TypeError: if size is not integer
                 ValueError: if size is negative
         """
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
-
         self.__size = size
 
     @property
