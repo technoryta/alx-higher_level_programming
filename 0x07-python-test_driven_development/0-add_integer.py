d8ac2d2a4162eaad6339fd44d0d4834f892a5b39
technoryta <technoryta@gmail.com>
2023-11-28 04:52:33 +0000

    update code

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+	"""adds two intergers
+	
+	args:
+		a: first integer
+		b: second integer
+	
+	return:
+		sum of a and b
+	"""
+	if not isinstance(a, int) and not isinstance(a, float):
+		raise TypeError("a must be an integer")
+	if not isinstance(b, int) and not isinstance (b, float):
+		raise TypeError("b must be an integer")
+	return (int(a) + int(b))
