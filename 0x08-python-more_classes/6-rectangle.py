698cdcf6e8bb5307935613c9a5ae7d427eaf8858
technoryta <technoryta@gmail.com>
2024-02-14 17:06:22 -0800

    updated script

---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+""" CLass defines rectangle """
+
+
+class Rectangle:
+    """Rectangle class"""
+    
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        """Initialisation
+        Args:
+            width: width of rect
+            height: height of rect
+        Raises:
+            TypeError: if inputs not int
+            ValueError: if inputs negative
+        """
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """ retrieves w"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """sets w"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """retrieves h"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """sets h"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2*(self.__width + self.__height)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        rectangle = ""
+        horizontal = '#' * self.__width
+        for i in range(self.__height):
+            rectangle += "\n" + horizontal
+        return (rectangle)
+
+    def __repr__(self):
+        return f"Rectangle({self.__width}, {self.height})"
+
+    def __del__(self):
+        print("Bye rectangle...")
+        if Rectangle.number_of_instances > 0:
+           Rectangle.number_of_instances -= 1
