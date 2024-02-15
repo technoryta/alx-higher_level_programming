010c8afddf05e5fec7aa736013d2633a94391da0
technoryta <technoryta@gmail.com>
2024-02-14 16:25:18 -0800

    updated script

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -63,4 +63,4 @@ class Rectangle:
         return (rectangle)
 
     def __repr__(self):
-        return "Rectangle({:d}, {:d})".fomat(self.__width, self.__height)
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
