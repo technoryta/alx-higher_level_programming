dba087f4a0c649fc30d2bb6b69a0f851cfc417a1
technoryta <technoryta@gmail.com>
2024-02-14 17:46:45 -0800

    updated script

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
-        Rectangle.number_of_instances += 1
+        self.number_of_instances += 1
 
     @property
     def width(self):
