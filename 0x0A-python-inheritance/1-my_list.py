a735c4a925da635733bfc4d5940c448ef0548099
technoryta <technoryta@gmail.com>
2024-02-21 18:42:23 -0800

    updated script

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""This module makes a class inherit from list"""
+"""This module makes a class inherit a list"""
 
 
 class MyList(list):
-    """This class inherits from list"""
+    """This class inherits a list"""
     def print_sorted(self):
         """prints sorted list"""
         print(sorted(self))
