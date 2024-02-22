55ccb6d5755fb076b46126420a10db00de786ddb
technoryta <technoryta@gmail.com>
2024-02-21 19:04:28 -0800

    updated script

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Module that checks if object is a class instance
+"""
+
+
+def is_same_class(obj, a_class):
+    """Returns true if object is class instance"""
+    return isinstance(obj, a_class)
