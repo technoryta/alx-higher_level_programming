56e9564cf085f6e01b9a84fa9c425688d41dabba
technoryta <technoryta@gmail.com>
2024-02-25 13:28:28 -0800

    updated script

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,4 +6,4 @@ def inherits_from(obj, a_class):
     """Returns true if object is inherits from a class
     otherwise, returns false
     """
-    return (issubclass(type(obj), a_class) and type(obj) != a_class)
+    return (isinstance(obj, a_class) and type(obj) != a_class)
