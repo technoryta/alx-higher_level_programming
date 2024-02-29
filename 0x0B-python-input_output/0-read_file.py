a8fda4a082f4e1c047600ba871d4e1f4320c3975
technoryta <technoryta@gmail.com>
2024-02-29 22:02:23 +0000

    update code

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A function that read text file UTF8"""
+
+
+def read_file(filename=""):
+	"""function reads and prints to stdout)"""
+	with open(filename) as f:
+		myfile = f.read()
+	for line in myfile:
+		print(line)
