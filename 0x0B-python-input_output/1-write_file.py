8ad1a5387239cb75547bfa892d54899df6eec80a
technoryta <technoryta@gmail.com>
2024-02-29 22:35:35 +0000

    update code

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,8 +2,7 @@
 """Writes string to a text"""
 
 
-
 def read_file(filename="", text=""):
 	"""function writes string to text"""
-	with open(filename, 'w') as f:
+	with open(filename, "w") as f:
 		return f.write(text)
