852701ff11fa1eedd620ec7d7935838a80d80584
technoryta <technoryta@gmail.com>
2024-03-01 04:07:55 -0800

    update code

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Writes Object to a Text File"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Saves to text using JSON"""
+    with open(filename) as f:
+        json.dump(myobj, filename)
